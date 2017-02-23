e06951a3d2dd930fc406b85933fb91387fce43e0
William Shiao <willshiao@gmail.com>
2017-02-23 11:16:09 -0800

    add music generation function

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const googleTTS = require('google-tts-api');
 const socketio = require('socket.io');
+const stringHash = require('string-hash');
 
+const noteList = require('./lib/notes.js');
+const MersenneTwister = require('mersennetwister');
 const index = require('./routes/index');
 
 const app = express();
+const mt = new MersenneTwister();
 const io = socketio();
 app.io = io;
 
@@ -24,7 +28,7 @@ app.set('view engine', 'hjs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -33,83 +37,99 @@ app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 io.on('connection', function(socket) {
-    console.log('Got connection.');
+  console.log('Got connection.');
 
-    socket.on('query', function(q) {
-        console.log(q);
-        wolframQuery(q, socket);
-    });
+  socket.on('query', function(q) {
+    console.log(q);
+    wolframQuery(q, socket);
+    generateNotes(q);
+  });
 });
 
 function wolframQuery(query, socket, url) {
-    let completed = false;
-    const timeout = 4; // How many seconds to wait before sending failure message
-    const failedQueryResponse = 'Ignorant human. Next time ask me a question that actually makes sense.';
-    const options = {
-        url: 'https://api.wolframalpha.com/v1/result?appid=XVVT6H-237WH8JWQY&i=' + encodeURI(query),
-        headers: {}
-    };
+  let completed = false;
+  const timeout = 4; // How many seconds to wait before sending failure message
+  const failedQueryResponse = 'Ignorant human. Next time ask me a question that actually makes sense.';
+  const options = {
+    url: 'https://api.wolframalpha.com/v1/result?appid=XVVT6H-237WH8JWQY&i=' + encodeURI(query),
+    headers: {}
+  };
 
-    function callback(error, response, body) {
+  function callback(error, response, body) {
 
-        // Handle successful response
-        if (!error && response.statusCode == 200) {
-            completed = true;
-            getAudioURL(body).then(function(url) {
-                socket.emit('message', {
-                    stuff: body,
-                    audioURL: url,
-                    success: false
-                });
-                console.log(body);
-            });
-        }
-
-        // Deal with failure
-        setTimeout(function() {
-            if (completed == false) {
-                getAudioURL(failedQueryResponse).then(function(url) {
-                    socket.emit('message', {
-                        stuff: failedQueryResponse,
-                        audioURL: url,
-                        success: false
-                    });
-                    console.log(`Request failed. Sending: ${failedQueryResponse}`);
-                });
-            }
-        }, timeout * 1000);
+    // Handle successful response
+    if (!error && response.statusCode == 200) {
+      completed = true;
+      getAudioURL(body).then(function(url) {
+        socket.emit('message', {
+          stuff: body,
+          audioURL: url,
+          success: false
+        });
+        console.log(body);
+      });
     }
 
-    request(options, callback);
+    // Deal with failure
+    setTimeout(function() {
+      if (completed == false) {
+        getAudioURL(failedQueryResponse).then(function(url) {
+          socket.emit('message', {
+            stuff: failedQueryResponse,
+            audioURL: url,
+            success: false
+          });
+          console.log('Request failed. Sending:', failedQueryResponse);
+        });
+      }
+    }, timeout * 1000);
+  }
+
+  request(options, callback);
 }
 
 function getAudioURL(text) {
-    return googleTTS(text, 'en', 1) // Speed = 1
-        .then(function(url) {
-            console.log(url);
-            return Promise.resolve(url);
-        })
-        .catch(function(err) {
-            console.error(err.stack);
-            return Promise.reject('Could not retrieve audio URL.');
-        });
+  return googleTTS(text, 'en', 1) // Speed = 1
+    .then(function(url) {
+      console.log(url);
+      return Promise.resolve(url);
+    })
+    .catch(function(err) {
+      console.error(err.stack);
+      return Promise.reject('Could not retrieve audio URL.');
+    });
+}
+
+function generateNotes(text) {
+  const notes = [];
+  const hash = stringHash(text);
+  console.log('Using hash value of', hash, 'for seed.');
+  mt.seed(hash);
+
+  for(let i = 0; i < text.length; ++i) {
+    let note = noteList[Math.round(mt.random() * noteList.length)];
+    note += ' e';
+    notes.push(note);
+  }
+  console.log('Generated notes:', notes);
+  return notes;
 }
 
 module.exports = app;
